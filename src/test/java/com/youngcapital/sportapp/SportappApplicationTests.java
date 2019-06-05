934147dc593069f56dca4db29f4be036662502a4
Lisette <lisettespiering@live.nl>
2019-06-05 09:55:29 +0200

    first commit

---
+++ src/test/java/com/youngcapital/sportapp/SportappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.youngcapital.sportapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SportappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
